ddd89a20498c117bb25320a17ff0a4114c54e043
Ryan Hefner <hi@ryanhefner.com>
2023-12-07 19:29:56 -0500

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json' assert { type: 'json' }
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    name: pkg.name,
+    file: './index.cjs',
+    format: 'umd',
+    globals: {
+      react: 'React',
+    },
+    banner: `/*! ${pkg.name} - ${pkg.version} !*/`,
+    footer: `/* Copyright ${(new Date()).getFullYear()} - ${pkg.author} */`,
+  },
+  external: [
+    'react',
+  ],
+  plugins: [
+    resolve({ extensions: ['.js', '.jsx'] }),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'runtime',
+    }),
+    commonjs(),
+    json(),
+  ],
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(terser())
+}
+
+export default config
